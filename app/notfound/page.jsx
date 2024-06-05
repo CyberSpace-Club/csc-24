bfb769605f841c2da1f1717f226e5354d188e77a
Kanishk <kanishkvalecha09@gmail.com>
2024-06-05 12:48:52 +0530

    Scroll bar fix

--- app/notfound/page.jsx
+++ app/notfound/page.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const fourofourpage = () => {
   return (
     <>
-      <div className="h-[100vh] w-[100vw] grid grid-cols-1 justify-center items-center coveragebg">
-        <h1 className="justify-self-center text-7xl text-[#eeeeee]">
+      <div className="h-[100vh] w-[100%] grid grid-cols-1 justify-center items-center coveragebg">
+        <h1 className="justify-self-center text:4xl lg:text-6xl text-[#eeeeee]">
           Coming Soon!
         </h1>
       </div>
--- app/previousevent/page.jsx
+++ app/previousevent/page.jsx
@@ -55,8 +55,8 @@ import React from "react";
 const fourofourpage = () => {
   return (
     <>
-      <div className="h-[100vh] w-[100vw] grid grid-cols-1 justify-center items-center coveragebg">
-        <h1 className="justify-self-center text-7xl text-[#eeeeee]">
+      <div className="h-[100vh] w-[100%] grid grid-cols-1 justify-center items-center coveragebg">
+        <h1 className="justify-self-center text-4xl lg:text-6xl m-[10%] text-[#eeeeee] ">
           Previous Events to be displayed......
         </h1>
       </div>
